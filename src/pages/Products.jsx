8bb95d97b01167aefba1860e47aa9146d750a4fa
TwirlDigiTech <twirldigitech@gmail.com>
2026-01-16 22:44:29 +0530

    Add files via upload

---
+++ src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const Products = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  )
+}
+
+export default Products
---
+++ src/pages/Services.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const Services = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  )
+}
+
+export default Services
